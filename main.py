e8bf489d12cfc615daa5245f52adfd947a27e504
jkdao <jasonkdao@gmail.com>
2019-12-27 20:01:32 -0800

    Revised comments throughout, removed unnecessary line breaks + code

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import sys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 
-def fillForm():
-    office = browser.find_element_by_id("officeId")  #
+
+def fillForm():  # Filling initial DMV form before checking for appointments according to config.py
+    office = browser.find_element_by_id("officeId")
     Select(office).select_by_visible_text(config.location)
     if config.testType == "auto":
         browser.find_element_by_id("DT").click()
@@ -28,7 +29,7 @@ def fillForm():
     browser.find_element_by_id("telSuffix").send_keys(config.phonenum1)
     browser.find_element_by_xpath('//*[@id="findOffice"]/fieldset/div[5]/input[2]').click()
 
-
+    # RECAPTCHA must be bypassed manually if it is found. 100 seconds given (checks for completion every 4 seconds)
     for number in range(25):
         try:
             browser.find_element_by_xpath('//*[@id="formId_1"]/div/div[2]/table/tbody/tr/td[2]/p[2]/strong')
@@ -40,6 +41,7 @@ def fillForm():
             pass
 
 
+# Checks if tempDate can already be found in date_list. If it is found, tempDate will be ignored
 def determineRepeated(tempDate, date_list):
     if (len(date_list) == 0):
         return False
@@ -52,6 +54,7 @@ def determineRepeated(tempDate, date_list):
         return False
 
 
+# Converts the str_datelist parsed from dateLog.txt to a list of date objects
 def convertDateStr(strdate_list):
     date_list = []
     for i in range(len(strdate_list)):
@@ -60,13 +63,15 @@ def convertDateStr(strdate_list):
     return date_list
 
 
+# Automates appointment booking after new closest appointment meets requirements
 def register(num):
     browser.find_element_by_xpath('//*[@id="app_content"]/div/a[1]').click()
     if num == 0:
-        print("Booking appointment on first run!")
+        print("Booking appointment on first run!")  # No "cancel previous appointment" if running for the first time
     else:
         print("Booking appointment..")
-        browser.find_element_by_xpath('// *[ @ id = "ApptForm"] / button').click()
+        browser.find_element_by_xpath(
+            '// *[ @ id = "ApptForm"] / button').click()  # Clicks "cancel previous appointment"
     try:
         browser.find_element_by_id("telephone_method").click()
     except NoSuchElementException:
@@ -76,9 +81,10 @@ def register(num):
     scrollTo = browser.find_element_by_xpath('//*[@id="app_content"]/div/p[1]/strong')
     browser.execute_script("arguments[0].scrollIntoView();", scrollTo)
     browser.save_screenshot('CurrentlyBooked.png')
-    print("Done! CHECK CURRENTLYBOOKED.PNG!")
+    print("Done! CHECK CURRENTLYBOOKED.PNG!")  # Saves confirmation screenshot to project directory
 
 
+# Checks to see if appointment date falls in desiredDateRange in config.py
 def dateFitsPreferences(tempDate):
     delta = tempDate - today()
     if delta.days <= config.desiredDateRange:
@@ -89,12 +95,14 @@ def dateFitsPreferences(tempDate):
 
 def search(date_list, closest):
     while True:
+        print(datetime.today().strftime("%I:%M:%S %p"))
         try:
             print(date_list)
-            strdate_list = [line.strip() for line in open("dateLog.txt", 'r')]
-            date_list = convertDateStr(strdate_list)
+            strdate_list = [line.strip() for line in
+                            open("dateLog.txt", 'r')]  # Converts dateLog.txt file to string list
+            date_list = convertDateStr(strdate_list)  # Converts string list to date list
 
-            if len(date_list) == 0:
+            if len(date_list) == 0:  # Empty date list = first run = no closest appointment
                 pass
             else:
                 closest = min(date_list)
@@ -104,27 +112,23 @@ def search(date_list, closest):
                 '%b %d, %Y at %I:%M %p').date()
             if dateFitsPreferences(tempDate):
                 if determineRepeated(tempDate, date_list):
-                    pass
+                    pass  # Ignore date because repeated
                 else:
                     print('New entry found! ////////////////////' + str(tempDate))
                     dateLog = open("dateLog.txt", "a")
                     dateLog.write(str(tempDate) + "\n")
-                    dateLog.flush()
-                    date_list.append(tempDate)
-                    if len(date_list) != 0:
-                        newclosest = min(date_list)
-                        if len(date_list) == 1:
-                            register(0)
-                            break
-                        else:
-                            if closest != newclosest:
-                                register(1)
-                                break
-            else:
-                pass
+                    dateLog.flush()  # dateLog updated via text file
+                    date_list.append(tempDate)  # dateLog also updated manually through append
+                    newclosest = min(date_list)  # no min
+                    if len(date_list) == 1:
+                        register(0)  # (0) occurs on first run, no previous appointment to cancel
+                        break
+                    else:
+                        if closest != newclosest:
+                            register(1)  # (1) occurs after first run, cancel previous appointment
+                            break  # Browser must be restarted, break out of while True
             time.sleep(config.refreshInterval)
-            print(date_list)
-            print("------------------")
+
             # "Refresh" Search
             Select(browser.find_element_by_id("requestedTime")).select_by_visible_text('8:00 AM')
             browser.find_element_by_id("checkAvail").click()
@@ -133,7 +137,10 @@ def search(date_list, closest):
             print("Webpage error. Refreshing...")
             time.sleep(config.refreshInterval)
             browser.refresh()
+        print("------------------")
 
+
+# ////////////////////////////////////////////////////////////////////////////////
 
 date_list = []
 closest = 0
